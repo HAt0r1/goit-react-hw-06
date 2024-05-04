c1e7d4dce5a6966399fe65a3166be4ec1f8deb2c
Hatori <ahgangadm@gmail.com>
2024-05-04 21:18:07 +0300

    Create redux store

---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { configureStore } from "@reduxjs/toolkit";
+import contactReducer from "./contactsSlice";
+
+export default configureStore({
+  reducer: {
+    contacts: contactReducer,
+  },
+});
