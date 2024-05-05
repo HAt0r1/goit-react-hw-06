00076510b9d9cb12d0ae91713c6142c85308ab57
Hatori <ahgangadm@gmail.com>
2024-05-05 17:25:42 +0300

    Add SearchBox component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,6 @@
 import ContactForm from "./ContactForm/ContactForm";
 import ContactList from "./ContactList/ContactList";
+import SearchBox from "./SearchBox/SearchBox";
 
 import style from "./App.module.css";
 
@@ -8,6 +9,7 @@ const App = () => {
     <div className={style.app}>
       <h1 className={style.title}>Phonebook</h1>
       <ContactForm />
+      <SearchBox />
       <ContactList />
     </div>
   );
